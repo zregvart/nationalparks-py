989a41d5762163c5e564c8b653c889449a5b0975
Zoran Regvart <zoran@regvart.com>
2017-02-27 12:33:30 +0100

    Updated display name once more

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'Earth National Parks (PY)',
+        'displayName': 'World National Parks (PY)',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
